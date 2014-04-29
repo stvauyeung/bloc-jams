c32b58254f3efeeb5c0e3804617e5a9699a24fec
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-04-29 02:40:56 +0000

    added document ready

--- app/scripts/practice.js
+++ app/scripts/practice.js
@@ -1,1 +1,11 @@
-$('p').css('font-size', '80px');
+$(document).ready(function() {
+  $('p').css('font-size', '80px');
+
+  var $funImage = $('img.fun-image');
+  var $imageContainer = $('.image-container');
+
+  for(var i = 0; i < 3; i++) {
+    var $clonedImage = $funImage.clone();
+    $clonedImage.appendTo($imageContainer);
+  }  
+});
