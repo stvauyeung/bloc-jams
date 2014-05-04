6024e6bb3b6afc94fbd6ec4aebca394480578b16
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-05-04 01:06:21 +0000

    attempt at album overlay

--- app/scripts/album_collection.js
+++ app/scripts/album_collection.js
@@ -1,3 +1,32 @@
+var buildAlbumOverlay = function(albumURL) {
+  var template = 
+      '<div class="collection-album-image-overlay">'
+    + '  <div class="collection-overlay-content">'
+    + '    <a class="collection-overlay-button" href="' + albumURL + '">'
+    + '      <i class="fa fa-play"></i>'
+    + '    </a>'
+    + '    &nbsp;'
+    + '    <a class="collection-overlay-button">'
+    + '      <i class="fa fa-plus"></i>'
+    + '    </a>'
+    + '  </div>'
+    + '</div>'
+    ;
+  return $(template);
+};
+
+var updateCollectionView = function() {
+  var onHover = function(event) {
+    $(this).append(buildAlbumOverlay('/album'))
+  };
+
+  var offHover = function(event) {
+    $(this).find('.collection-album-image-overlay').remove();
+  };
+  
+  var $newThumbnail = $('.album-thumbnail');
+  $newThumbnail.children('.collection-album-image-container').hover(onHover, offHover);
+};
 
 if (document.URL.match(/\/collection/)) {
   // Wait until the HTML is fully processed.
