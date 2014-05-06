f0d61009d3d61ba11e27e67a19fe3b0b9f346911
Stephen Auyeung <auyeung.chi.kit@gmail.com>
2014-05-06 04:12:18 +0000

    set up subscription tab

--- app/scripts/user_profile.js
+++ app/scripts/user_profile.js
@@ -1,5 +1,11 @@
 
 if (document.URL.match(/\/user/)) {
   $(document).ready(function() {
+    $('.tab').click(function(){
+      $('#tabs-container > .tab.active').removeClass('active');
+      $(this).addClass('active');
+      $('.tab-contents-container > .tab-contents.tab-contents-active').removeClass('tab-contents-active');
+      $(this.rel).addClass('tab-contents-active');
+    });
   });
 }
